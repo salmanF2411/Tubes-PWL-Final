61a71cd02457adda9c3b49a3cc3881d4cc1025d2
Dwiky-1717 <dwikyc54@gmail.com>
2026-06-14 22:16:40 +0700

    Sidebar KelolaUser

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -30,5 +30,46 @@
                 <span>Stok</span>
             </a>
         </li>
+        <li>
+            <button onclick="toggleReports()"
+                class="w-full flex items-center gap-3 px-4 py-3 rounded-lg font-medium transition-all duration-200 cursor-pointer text-slate-700 hover:bg-[#dbeafe] hover:text-[#062f5f] hover:shadow-sm">
+
+                <i class="fa-solid fa-chart-bar w-5"></i>
+                <span>Laporan</span>
+
+                <i class="fa-solid fa-chevron-down w-4 ml-auto text-xs transition-transform duration-200"
+                    id="reportChevron">
+                </i>
+            </button>
+
+            <ul id="reportsMenu"
+                class="space-y-1 ml-6 mt-1 border-l border-slate-200 pl-3 hidden">
+
+                <li>
+                    <a href="#"
+                        class="flex items-center gap-3 px-4 py-2 rounded font-medium transition-all duration-200 text-sm text-slate-700 hover:bg-[#dbeafe] hover:text-[#062f5f] hover:shadow-sm">
+
+                        <i class="fa-solid fa-receipt w-4"></i>
+                        <span>Laporan Transaksi</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="#"
+                        class="flex items-center gap-3 px-4 py-2 rounded font-medium transition-all duration-200 text-sm text-slate-700 hover:bg-[#dbeafe] hover:text-[#062f5f] hover:shadow-sm">
+
+                        <i class="fa-solid fa-boxes-stacked w-4"></i>
+                        <span>Laporan Stok</span>
+                    </a>
+                </li>
+            </ul>
+        </li>
+        <li>
+            <a href="#"
+                class="flex items-center gap-3 px-4 py-3 rounded-lg font-medium transition-all duration-200 text-slate-700 hover:bg-[#dbeafe] hover:text-[#062f5f] hover:shadow-sm">
+                <i class="fa-solid fa-users-gear w-5"></i>
+                <span>Kelola User</span>
+            </a>
+        </li>
     </ul>
 </aside>
