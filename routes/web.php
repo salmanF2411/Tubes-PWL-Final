391c30df41672a5696288fe2a854a8f166824209
salmanF2411 <salman241105@gmail.com>
2026-06-16 11:48:43 +0700

    Menambahkan Route Halaman Produk

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('pages.dashboard');
 });
+
+Route::get('/produk', function () {
+    return view('pages.produk');
+});
+
