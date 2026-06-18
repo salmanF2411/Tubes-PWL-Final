50d4a3f5b287a8ef6fe3c7fe2b59388253865f5a
salmanF2411 <salman241105@gmail.com>
2026-06-18 11:48:51 +0700

    Membuat Model TransactionsItem

---
+++ app/Models/TransactionItem.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class TransactionItem extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'transaction_id',
+        'product_id',
+        'quantity',
+        'price',
+        'subtotal',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'quantity' => 'integer',
+            'price' => 'decimal:2',
+            'subtotal' => 'decimal:2',
+        ];
+    }
+
+    public function transaction(): BelongsTo
+    {
+        return $this->belongsTo(Transaction::class);
+    }
+
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class);
+    }
+}
