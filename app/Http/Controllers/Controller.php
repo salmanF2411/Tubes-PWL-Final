71a6df2526d0c171251567b781969f95c5e82a64
salmanF2411 <salman241105@gmail.com>
2026-06-18 12:09:34 +0700

    mengedit Controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
 abstract class Controller
 {
-    //
+    protected function currentUser(): User
+    {
+        return Auth::user();
+    }
+
+    protected function visibleStoreIds(): array
+    {
+        return $this->currentUser()->visibleStoreIds();
+    }
+
+    protected function ensureVisibleStore(int $storeId): int
+    {
+        abort_unless(in_array($storeId, $this->visibleStoreIds(), true), 403);
+
+        return $storeId;
+    }
 }
